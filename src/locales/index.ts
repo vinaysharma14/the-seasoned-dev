2fd4c86c96dc724608a594a5d1a629efe43cafa9
Vinay Sharma <vinaysharma7811@gmail.com>
2021-02-21 17:38:56 +0530

    chore: removed unused locales

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,13 +1,7 @@
 import en from './en.json';
 import fr from './fr.json';
-import de from './de.json';
-import nl from './nl.json';
-import da from './da.json';
 
 export const messages = {
   en,
   fr,
-  de,
-  nl,
-  da,
 };
