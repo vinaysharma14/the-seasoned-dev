da4d55a71ea964745ab1d04dc3ac9c0cc9b86ab1
Vinay Sharma <vinaysharma7811@gmail.com>
2021-02-21 18:41:35 +0530

    feature: suspense fallback designd with text animation

--- src/fallbacks/Suspense/index.tsx
+++ src/fallbacks/Suspense/index.tsx
@@ -1,9 +1,22 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+
+import { useTheme } from 'hooks';
 
 import './styles.scss';
 
-export const SuspenseFallback: FC = () => (
-  <div>
-    <p>Suspense</p>
-  </div>
-);
+export const SuspenseFallback: FC = () => {
+  const { theme } = useTheme();
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => setAnimate(true), []);
+
+  return (
+    <div className={`container ${theme}`}>
+      <h1 className={animate ? 'animate' : ''}>TH</h1>
+      <h1 className={animate ? 'animate' : ''}>E&nbsp;</h1>
+      <h1 className={animate ? 'animate' : ''}>SEASON</h1>
+      <h1 className={animate ? 'animate' : ''}>ED&nbsp;</h1>
+      <h1 className={animate ? 'animate' : ''}>DEV</h1>
+    </div>
+  );
+};
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,7 +3,7 @@ import { FC } from 'react';
 type LazyImport = Promise<{ default: FC<unknown> }>;
 
 const delayImport = (lazyImport: LazyImport): LazyImport => Promise.all([
-  lazyImport, new Promise((resolve) => setTimeout(resolve, 2000)),
+  lazyImport, new Promise((resolve) => setTimeout(resolve, 2500)),
 ]).then(([moduleExports]) => moduleExports);
 
 export { delayImport };
