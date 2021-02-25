4cef7181043aabc3ca373c0894b35e6c92556af6
Vinay Sharma <vinaysharma7811@gmail.com>
2021-02-25 23:19:04 +0530

    feature: added theme color to body

--- src/hooks/theme.ts
+++ src/hooks/theme.ts
@@ -1,8 +1,14 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+
 import { RootState } from 'store/types';
 
 export const useTheme = (): { theme: 'light' | 'dark' } => {
   const { theme } = useSelector(({ ui }: RootState) => ui);
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
 
   return { theme };
 };
